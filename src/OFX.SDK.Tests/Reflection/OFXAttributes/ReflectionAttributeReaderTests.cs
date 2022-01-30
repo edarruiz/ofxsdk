53c334081bebd2550562fb81d056cddee84e0174
Eric Darruiz <33941948+edarruiz@users.noreply.github.com>
2022-01-30 10:02:27 -0300

    Add OFXTagAttribute reader

--- src/OFX.SDK.Tests/Reflection/OFXAttributes/ReflectionAttributeReaderTests.cs
+++ src/OFX.SDK.Tests/Reflection/OFXAttributes/ReflectionAttributeReaderTests.cs
@@ -97,20 +97,72 @@ public class ReflectionAttributeReaderTests {
         // Assert
         Assert.Equal(expected, result);
     }
+
+    [Fact(DisplayName = "Get OFXTagAttibute value from Enum fields")]
+    public void Get_OFXTagAttribute_FromEnumFields() {
+        // Arrange
+        var firstValue = DummyEnum.FirstValue;
+        var secondValue = DummyEnum.SecondValue;
+        var thirdValue = DummyEnum.ThirdValue;
+        var expectedHeaderTag = "FIRSTVALUE:";
+        var expectedTagSecondValue = "<SECONDVALUE>";
+        var expectedTagThirdValue = "<THIRDVALUE>";
+
+        // Act
+        var resultHeaderTag = firstValue.GetOFXTagName();
+        var resultTagSecondValue = secondValue.GetOFXTagName();
+        var resultTagThirdValue = thirdValue.GetOFXTagName();
+
+        // Assert
+        Assert.Equal(expectedHeaderTag, resultHeaderTag);
+        Assert.Equal(expectedTagSecondValue, resultTagSecondValue);
+        Assert.Equal(expectedTagThirdValue, resultTagThirdValue);
+    }
+
+    [Fact(DisplayName = "Get OFXTagAttribute value from Class properties")]
+    public void Get_OFXTagAttribute_FromClassProperties() {
+        // Arrange
+        var actual = new DummyClassWithVersion();
+        var expectedHeaderTag = "Header Tag:";
+        var expectedTag1 = "<SomeProperty>";
+        var expectedTag2 = "<OtherProperty>";
+
+        // Act
+        var resultHeaderTag = actual.GetOFXTagName(nameof(actual.PropertyWithHeaderTag));
+        var resultTag1 = actual.GetOFXTagName(nameof(actual.SomeProperty));
+        var resultTag2 = actual.GetOFXTagName(nameof(actual.OtherProperty));
+
+        // Assert
+        Assert.Equal(expectedHeaderTag, resultHeaderTag);
+        Assert.Equal(expectedTag1, resultTag1);
+        Assert.Equal(expectedTag2, resultTag2);
+    }
     #endregion
 }
 
 [OFXVersion(OFXSpecification.Version102)]
 public enum DummyEnum {
     [OFXValue("#1 Value")]
+    [OFXTag(OFXSpecification.Version102, "FIRSTVALUE:", true)]
     FirstValue = 0,
 
     [OFXValue("#2 Value")]
+    [OFXTag(OFXSpecification.Version102, "SECONDVALUE")]
     SecondValue = 1,
 
     [OFXValue("#3 Value")]
+    [OFXTag(OFXSpecification.Version102, "THIRDVALUE")]
     ThirdValue = 2
 }
 
 [OFXVersion(OFXSpecification.Version102)]
-public class DummyClassWithVersion { }
+public class DummyClassWithVersion {
+    [OFXTag(OFXSpecification.Version102, "Header Tag:", true)]
+    public string? PropertyWithHeaderTag { get; set; }
+
+    [OFXTag(OFXSpecification.Version102, "SomeProperty")]
+    public string? SomeProperty { get; set; }
+
+    [OFXTag(OFXSpecification.Version102, "OtherProperty")]
+    public int OtherProperty { get; set; }
+}
--- src/OFX.SDK/Reflection/ReflectionAttributeReader.cs
+++ src/OFX.SDK/Reflection/ReflectionAttributeReader.cs
@@ -39,41 +39,83 @@ namespace OFX.SDK.Reflection;
 /// Represents the reflection attributes reader for any kinds of objects.
 /// </summary>
 public static class ReflectionAttributeReader {
+    #region Helpers
+    /// <summary>
+    /// Returns the information from an attribute annotation applied to any object field or property.
+    /// </summary>
+    /// <typeparam name="TSource">The generic type having the attribute annotation.</typeparam>
+    /// <typeparam name="TAttribute">The annotation attribute class reference.</typeparam>
+    /// <param name="source">The generic type having the attribute annotation.</param>
+    /// <param name="propertyName">Name of the property which the attribute annotation is applied.</param>
+    /// <returns>Returns if found, the attribute information from the field when the type is an <see cref="Enum"/>,
+    /// or from the property name informed when the source type is a <see langword="class"/> or a <see langword="struct"/>.</returns>
+    private static TAttribute? GetAttributeInfo<TSource, TAttribute>(TSource source, string? propertyName = null) where TAttribute : Attribute {
+        ArgumentNullException.ThrowIfNull(source, nameof(source));
+
+        TAttribute? attribute = null;
+        var type = source.GetType();
+        if (type.IsEnum) { // enum
+#pragma warning disable CS8604 // Possible null reference argument.
+            FieldInfo? fieldInfo = source.GetType().GetField(source.ToString());
+#pragma warning restore CS8604 // Possible null reference argument.
+            attribute = fieldInfo?.GetCustomAttribute<TAttribute>(false);
+        } else if ((type.IsClass) || (type.IsValueType && !type.IsPrimitive)) { // class or struct
+#pragma warning disable CS8604 // Possible null reference argument.
+            PropertyInfo? propertyInfo = source.GetType().GetProperty(propertyName);
+#pragma warning restore CS8604 // Possible null reference argument.
+            attribute = propertyInfo?.GetCustomAttribute<TAttribute>(false);
+        }
+
+        return attribute;
+    }
+    #endregion
+
     #region OFXValueAttribute Reader
     /// <summary>
-    /// Gets the <see cref="OFXValueAttribute"/> value from the object annotation.
+    /// Gets the value from the <see cref="OFXValueAttribute"/> annotation applied to the object.
     /// </summary>
     /// <typeparam name="T">The generic type having the <see cref="OFXValueAttribute"/> annotation.</typeparam>
     /// <param name="source">Instance of the generic type having the <see cref="OFXValueAttribute"/> annotation.</param>
-    /// <returns>Returns the value assigned to <see cref="OFXValueAttribute"/> annotation.</returns>
+    /// <returns>Returns the value assigned to the <see cref="OFXValueAttribute"/> annotation applied to the object.</returns>
     public static string? GetOFXValue<T>(this T source) {
         ArgumentNullException.ThrowIfNull(source, nameof(source));
 
-#pragma warning disable CS8604 // Possible null reference argument.
-        FieldInfo? fi = source.GetType().GetField(source.ToString());
-#pragma warning restore CS8604 // Possible null reference argument.
-        if (fi is null) {
-            return null;
-        }
-        var attribute = fi.GetCustomAttribute<OFXValueAttribute>(false);
+        var result = GetAttributeInfo<T, OFXValueAttribute>(source);
 
-        return (attribute is null) ? null : attribute.Value as string;
+        return (result is null) ? null : result.Value as string;
     }
     #endregion
 
     #region OFXVersionAttribute Reader
     /// <summary>
-    /// Gets the <see cref="OFXVersionAttribute"/> value from the object annotation.
+    /// Gets the <see cref="OFXSpecification"/> value from the <see cref="OFXVersionAttribute"/> annotation
+    /// applied to the object.
     /// </summary>
-    /// <typeparam name="T">The generic type  having the <see cref="OFXVersionAttribute"/> annotation.</typeparam>
+    /// <typeparam name="T">The generic type having the <see cref="OFXVersionAttribute"/> annotation.</typeparam>
     /// <param name="source">Instance of the generic type having the <see cref="OFXVersionAttribute"/> annotation.</param>
-    /// <returns>Returns the value assigned to <see cref="OFXVersionAttribute"/> annotation.</returns>
+    /// <returns>Returns the <see cref="OFXSpecification"/> value assigned to the <see cref="OFXVersionAttribute"/> object annotation.</returns>
     public static OFXSpecification GetOFXVersion<T>(this T source) {
         ArgumentNullException.ThrowIfNull(source, nameof(source));
 
         var attribute = source.GetType().GetCustomAttribute<OFXVersionAttribute>(false);
 
         return (attribute is null) ? OFXSpecification.Unknown : attribute.Specification;
+    }
+    #endregion
+
+    #region OFXTagAttribute Reader
+    /// <summary>
+    /// Gets the tag name from the <see cref="OFXTagAttribute"/> annotation applied to the object.
+    /// </summary>
+    /// <typeparam name="T">The generic type having the <see cref="OFXTagAttribute"/> annotation.</typeparam>
+    /// <param name="source">Instance of the generic type having the <see cref="OFXTagAttribute"/> annotation.</param>
+    /// <param name="propertyName">Name of the property annotated.</param>
+    /// <returns>Returns the tag name of the <see cref="OFXTagAttribute"/> assigned to the object annotation.</returns>
+    public static string? GetOFXTagName<T>(this T source, string? propertyName = null) {
+
+        var result = GetAttributeInfo<T, OFXTagAttribute>(source, propertyName);
+
+        return result?.Name;
     }
     #endregion
 }
