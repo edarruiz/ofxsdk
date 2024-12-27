dd85a510b51af05a0b77e00891c783d04daca082
Eric Darruiz <33941948+edarruiz@users.noreply.github.com>
2024-12-27 20:10:13 -0300

    feat: Create SGMLTag.cs

---
+++ src/OFX.SDK/IO/SGMLTag.cs
@@ -0,0 +1,52 @@
+using System.Diagnostics;
+
+namespace OFX.SDK;
+
+#region MIT License Information
+/*
+ *
+ * MIT License
+ *
+ * Copyright (c) 2020-2025 Eric Roberto Darruiz
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+#endregion
+
+/// <summary>
+/// Represents a tag in a SGML file.
+/// </summary>
+[DebuggerDisplay("Name = {Name}, Value = {Value}, Level = {Level}")]
+internal struct SGMLTag {
+    /// <summary>
+    /// The name of the tag.
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// The value of the tag.
+    /// </summary>
+    public string Value { get; set; }
+
+    /// <summary>
+    /// The level of the tag.
+    /// </summary>
+    public int Level { get; set; }
+}
