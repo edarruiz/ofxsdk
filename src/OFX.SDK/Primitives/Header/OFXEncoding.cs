2acbf158505cc10e6ad7bbed55dff98af3501ec6
Eric Darruiz <33941948+edarruiz@users.noreply.github.com>
2024-12-27 16:45:01 -0300

    Create OFXEncoding.cs

---
+++ src/OFX.SDK/Primitives/Header/OFXEncoding.cs
@@ -0,0 +1,15 @@
+namespace OFX.SDK.Primitives.Header;
+
+/// <summary>
+/// Represents the OFX text encoding used for character data.
+/// </summary>
+public enum OFXEncoding {
+    /// <summary>
+    /// Represents the Unicode text encoding.
+    /// </summary>
+    UTF8 = 0,
+    /// <summary>
+    /// Represents the US ASCII text encoding.
+    /// </summary>
+    USASCII
+}
