9ac2533d9e7e3f4c9e081fd9f965d4d23ac77f57
Eric Darruiz <33941948+edarruiz@users.noreply.github.com>
2024-12-27 15:14:41 -0300

    Create OFXFooterTagAttribute.cs

---
+++ src/OFX.SDK/DataAnnotations/OFXFooterTagAttribute.cs
@@ -0,0 +1,47 @@
+namespace OFX.SDK.DataAnnotations;
+
+#region MIT License Information
+/*
+ *
+ * MIT License
+ *
+ * Copyright (c) 2020-2025 Eric Roberto Darruiz
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+#endregion
+
+/// <summary>
+/// Represents an OFX tag for any OFX footer structure declarations.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Property | AttributeTargets.Struct | AttributeTargets.Enum | AttributeTargets.Field, Inherited = false, AllowMultiple = true)]
+public sealed class OFXFooterTagAttribute : OFXAbstractTagAttribute {
+    #region Ctor
+    /// <summary>
+    /// Initializes a new instance of the <see cref="OFXFooterTagAttribute"/> class with a specified name.
+    /// </summary>
+    /// <param name="name">The name of the OFX footer tag.</param>
+    /// <exception cref="ArgumentException">When the <paramref name="name"/> is <see langword="null"/>, empty, 
+    /// or consists only of white-space characters.</exception>
+    public OFXFooterTagAttribute(string name) : base(name) {
+        Scope = OFXTagScope.Footer;
+    }
+    #endregion
+}
