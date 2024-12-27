88d74f47ec569ccfbf819bf5dda250e15d6bcaa9
Eric Darruiz <33941948+edarruiz@users.noreply.github.com>
2024-12-27 16:45:04 -0300

    Create OFXSecurity.cs

---
+++ src/OFX.SDK/Primitives/Header/OFXSecurity.cs
@@ -0,0 +1,15 @@
+namespace OFX.SDK.Primitives.Header;
+
+/// <summary>
+/// Represents the OFX application-level security.
+/// </summary>
+public enum OFXSecurity {
+    /// <summary>
+    /// Represents no application-level security.
+    /// </summary>
+    None = 0,
+    /// <summary>
+    /// Represents Type 1 application-level security.
+    /// </summary>
+    Type1
+}
